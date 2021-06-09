2315fb5774cd4571c38a793b8e7ae32c905509e2
rattanaksar <81554110+rattanaksar@users.noreply.github.com>
2021-06-09 10:41:39 +0200

    Exo 3 Php

---
+++ Exo 3.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo 3 : Les variables PHP</title>
+</head>
+<body>
+    <h1>Exo 3 Partie : Les variables PHP</h1>
+    <?php
+        $km = 1;
+        echo $km;
+        echo "<br>";
+        $km= 3;
+        echo $km;
+        echo "<br>";
+        $km=125;
+        echo $km;
+        echo "<br>";
+    ?>
+</body>
+</html>
